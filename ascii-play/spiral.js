13b379aad0b9ef77f41f48932ecfcaf133ce5b44
Shelby Wilson <s.wilson024@gmail.com>
2025-08-18 22:57:33 -0700

    update about copy; adjust spiral

--- ascii-play/spiral.js
+++ ascii-play/spiral.js
@@ -1,12 +1,12 @@
 export function main(coord, context, cursor, buffer) {
-	const density = '.SPAM...'
+	const density = '.SPAM.        '
 	const t = context.time * 0.001
 	const m = Math.min(context.cols, context.rows)
 	const a = context.metrics.aspect
 
 	const st = {
-		x: 2.0 * (coord.x - context.cols / 2) / m * a,
-		y: 2.0 * (coord.y - context.rows / 2) / m
+		x: 2 * (coord.x - context.cols / 2) / m * a,
+		y: 2 * (coord.y - context.rows / 2) / m
 	}
 
 	const radius = Math.sqrt(st.x * st.x + st.y * st.y)
@@ -15,9 +15,9 @@ export function main(coord, context, cursor, buffer) {
 	const spirals = 5
 	const spiralOffset = angle * spirals + radius * 8 - t * 2
 
-	const spiralValue = Math.sin(spiralOffset) * 0.2 + 0.5
+	const spiralValue = Math.sin(spiralOffset) * 0.25 + 0.15
 
-	const falloff = Math.exp(-radius * 1.9)
+	const falloff = Math.exp(-radius * 1.8)
 	const c = spiralValue * falloff
 
 	const index = Math.floor(c * density.length)
